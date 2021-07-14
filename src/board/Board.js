224965f051a8c8243aa6ec4766f89a32f6982bed
Jenny Ly <lycb@appstate.edu>
2021-07-14 14:07:28 -0400

    initial commit for jeopardy

---
+++ src/board/Board.js
@@ -0,0 +1,13 @@
+import './Board.css';
+
+function Board() {
+  return (
+    <div className="Board">
+      <header className="Board-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default Board;

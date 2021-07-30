63afe4fff265b959bf412f99ff25c5cb0a5febee
Jenny Ly <lycb@appstate.edu>
2021-07-29 20:09:04 -0400

    update iframe to have music

--- src/pages/board/board.js
+++ src/pages/board/board.js
@@ -6,7 +6,7 @@ function Board() {
   return (
     <div>
       <h1>Board</h1>
-      <iframe src="coffin.mp3" title="coffin music" allow="autoplay;loop" id="iframeAudio" className="mp3Player"></iframe>
+      <iframe className="musicPlayer" src="https://www.youtube.com/embed/j9V78UbdzWI?loop=1&playlist=j9V78UbdzWI&autoplay=1" title="Coffin Dance Song"></iframe>
       <GameBoard />
     </div>
   );

3f99cfb00d3f63257dfdb36d8dbfc5c0a5ab9de2
Jenny Ly <lycb@appstate.edu>
2021-10-31 20:40:51 -0400

    add obj

---
+++ src/board_object.js
@@ -0,0 +1,205 @@
+export let board_obj =
+{
+   "gameName": "string",
+   "createdBy": "string",
+   "board": [
+      // Category 1 board [0]
+      [
+         {
+            "point": 100,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 200,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 300,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 400,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 500,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 600,
+            "question": "question",
+            "answer": "answer"
+         }
+      ],
+      // Category 2 board [1]
+      [
+         {
+            "point": 100,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 200,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 300,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 400,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 500,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 600,
+            "question": "question",
+            "answer": "answer"
+         }
+      ],
+      // Category 3 board [2]
+      [
+         {
+            "point": 100,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 200,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 300,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 400,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 500,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 600,
+            "question": "question",
+            "answer": "answer"
+         }
+      ],
+      // Category 4 board [3]
+      [
+         {
+            "point": 100,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 200,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 300,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 400,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 500,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 600,
+            "question": "question",
+            "answer": "answer"
+         }
+      ],
+      // Category 5 board [4]
+      [
+         {
+            "point": 100,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 200,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 300,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 400,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 500,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 600,
+            "question": "question",
+            "answer": "answer"
+         }
+      ],
+      // Category 6 board [5]
+      [
+         {
+            "point": 100,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 200,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 300,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 400,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 500,
+            "question": "question",
+            "answer": "answer"
+         },
+         {
+            "point": 600,
+            "question": "question",
+            "answer": "answer"
+         }
+      ],
+   ],
+}

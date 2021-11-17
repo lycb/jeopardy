74b922ebf3cd5b960cbfb6abad4049685df8457b
Jenny Ly <jennyly294@gmail.com>
2021-11-16 21:09:02 -0500

    turn indicator

--- src/pages/board/board.js
+++ src/pages/board/board.js
@@ -4,6 +4,24 @@ import ScoreBoard from '../../components/scoreboard/scoreboard';
 import './board.css';
 
 export default class Board extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      "turnIndex": 0
+    }
+
+    this.onAnswered = this.onAnswered.bind(this);
+  }
+
+  onAnswered(category, index, teamindex) {
+    if (parseInt(teamindex,10) !== -1) {
+      this.setState({ turnIndex: parseInt(teamindex,10) });
+    }
+    
+    this.props.onAnswered(category, index);
+  }
+
   render() {
     const scoreUnit = this.props.scoreUnit
     const teams = this.props.teams
@@ -11,9 +29,8 @@ export default class Board extends Component {
 
     return (
       <div>
-        <h1>Board</h1>
         <div className="top-board">
-          <iframe width="400" height="100" src="https://www.youtube.com/embed/videoseries?list=PLql_LE4BdRHebM1tp0-XnsIQxVrpSnrTe&autoplay=1&loop=1&mute=1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          {teams.length > 0 ? <h1 className="turn-indicator">It's team {teams[this.state.turnIndex].name ? teams[this.state.turnIndex].name : this.state.turnIndex + 1}'s turn!</h1> : null}
           <ScoreBoard
             scoreUnit={scoreUnit}
             teams={teams}/>
@@ -22,8 +39,11 @@ export default class Board extends Component {
           scoreUnit={scoreUnit}
           teams={teams}
           board={board} 
-          onAnswered={this.props.onAnswered}
+          onAnswered={this.onAnswered}
           onPointsChange={this.props.onPointsChange}/>
+        <div className="bot-board">
+          <iframe width="400" height="100" src="https://www.youtube.com/embed/videoseries?list=PLql_LE4BdRHebM1tp0-XnsIQxVrpSnrTe&autoplay=1&loop=1&mute=1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        </div>
       </div>
     );
   }
